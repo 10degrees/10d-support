eececc2767e66f7d5f770c3aaaf18f376660be60
Alice Williams <alice@10degrees.uk>
2020-09-07 16:57:04 +0100

    Set version to 1.4.0

--- 10d-wordcare-report.php
+++ 10d-wordcare-report.php
@@ -2,7 +2,7 @@
 /*
 * Plugin Name: 10 Degrees WordCare
 * Description: WordPress management and reporting for 10 Degrees WordCare clients.
-* Version: 1.3.0
+* Version: 1.4.0
 * Author: 10 Degrees
 * Author URI: https://www.10degrees.uk
 * Github Plugin URI: https://github.com/10degrees/10d-wordcare-report
