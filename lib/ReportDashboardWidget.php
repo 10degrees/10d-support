af400e4259720ba108386e6bfc275ccd836d8673
Jon Benham <jon@10degrees.uk>
2021-05-18 10:24:56 +0100

    added wordcare contact details to dashboard widget

--- lib/ReportDashboardWidget.php
+++ lib/ReportDashboardWidget.php
@@ -6,6 +6,19 @@ function td_plugin_report_dashboard_display()
 {
     $NewTestRequired = null;
 
+    //WordCare Contact Details
+    echo '<h3>WordCare Contact Details</h3>
+        <h4>Support available Monday-Friday 9am-5pm</h4>
+        <p class="td_contact_details">
+            <svg class="tend-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M21 16.42v3.536a1 1 0 0 1-.93.998c-.437.03-.794.046-1.07.046-8.837 0-16-7.163-16-16 0-.276.015-.633.046-1.07A1 1 0 0 1 4.044 3H7.58a.5.5 0 0 1 .498.45c.023.23.044.413.064.552A13.901 13.901 0 0 0 9.35 8.003c.095.2.033.439-.147.567l-2.158 1.542a13.047 13.047 0 0 0 6.844 6.844l1.54-2.154a.462.462 0 0 1 .573-.149 13.901 13.901 0 0 0 4 1.205c.139.02.322.042.55.064a.5.5 0 0 1 .449.498z" fill="#000"/></svg>
+            Call us: <a href="tel:01183913910">0118 391 3910</a>
+        </p>
+        <p class="td_contact_details">
+            <svg class="tend-icon" width="24" height="24" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M20,4 L4,4 C2.9,4 2.01,4.9 2.01,6 L2,18 C2,19.1 2.9,20 4,20 L20,20 C21.1,20 22,19.1 22,18 L22,6 C22,4.9 21.1,4 20,4 Z M19.6,8.25 L12.53,12.67 C12.21,12.87 11.79,12.87 11.47,12.67 L4.4,8.25 C4.15,8.09 4,7.82 4,7.53 C4,6.86 4.73,6.46 5.3,6.81 L12,11 L18.7,6.81 C19.27,6.46 20,6.86 20,7.53 C20,7.82 19.85,8.09 19.6,8.25 Z" id="🔹Icon-Color" fill="#1D1D1D"></path></svg>
+            Email us: <a href="mailto:support@10degrees.uk">support@10degrees.uk</a>
+        </p>
+        <hr />';
+
     // Table for WordPress Plugin Updates Log
     if ($log = get_option('td_plugin_update_log')) {
         $count = count($log);
@@ -19,7 +32,7 @@ function td_plugin_report_dashboard_display()
 
     // Table for WordPress Core Updates Log
     if (!get_option('td_core_update_log')) {
-        echo 'No WordPress Core Updates in the log at the moment.';
+        echo '<p>No WordPress Core Updates in the log at the moment.</p>';
     } else {
         $log = get_option('td_core_update_log');
         $count = count($log);
