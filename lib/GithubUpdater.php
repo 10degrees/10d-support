f473bd5a846e1b4045308e2de221b52be8a48097
Alice Williams <alice@10degrees.uk>
2022-02-04 15:37:04 +0000

    Use new access token. Show settings page if using 10degrees.uk email.

--- lib/GithubUpdater.php
+++ lib/GithubUpdater.php
@@ -4,11 +4,25 @@
  */
 add_filter('github_updater_set_options', function () {
     return [
-        '10d-wordcare-report'    => 'e24779cb075222911d8477559c3211996ae77360',
+        '10d-wordcare-report'    => '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',
     ];
 });
 
 /**
  * Remove Github Updater settings page
  */
-add_filter('github_updater_hide_settings', '__return_true');
+add_filter('github_updater_hide_settings', function() {
+    $current_user = wp_get_current_user();
+
+    if (!$current_user) {
+        return true;
+    }
+
+    $has_10degrees_email = strpos($current_user->user_email, '@10degrees.uk');
+
+    if ($has_10degrees_email === false) {
+        return true;
+    }
+
+    return false;
+});
