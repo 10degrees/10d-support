25a1c2720cefd33e8b46b9eecc3389a2e350d180
Jon Benham <jon@10degrees.uk>
2025-07-22 09:18:44 +0100

    update github repo url

--- lib/PluginUpdateChecker.php
+++ lib/PluginUpdateChecker.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../plugin-update-checker/plugin-update-checker.php';
 use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
 
 $UpdateChecker = PucFactory::buildUpdateChecker(
-    'https://github.com/10degrees/10d-wordcare-report',
+    'https://github.com/10degrees/10d-support',
     TEND_PLUGIN_PATH . '10d-support-report.php',
     '10d-support-report'
 );
